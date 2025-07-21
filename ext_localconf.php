f0f8770c43f51a92b64f3099ed4ee3274a923620
Eike Drost <eike.drost@mceikens.de>
2025-07-21 19:06:50 +0200

    Store StoragePids configuration for roles and permissions in GLOBALS TYPO3_CONF_VARS
    Moved the StoragePids settings for roles and permissions into the TYPO3 global configuration to centralize and standardize their management.

---
+++ ext_localconf.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!defined('TYPO3')) {
+    die('Access denied.');
+}
+
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['phlabc']['storage']['role'] = 1;
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['phlabc']['storage']['permission'] = 1;
