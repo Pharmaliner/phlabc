fce7c2a0ae8dc817441a8bad1bf220b083dfba00
Eike Drost <eike.drost@mceikens.de>
2026-01-26 21:02:23 +0100

    add ext_emconf for TER release

---
+++ ext_emconf.php
@@ -0,0 +1,25 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Pharmaline ABC',
+    'description' => 'Pharmaline Extension to manage access to objects, content and actions with permissions.',
+    'category' => 'fe',
+    'author' => 'Christian Platt, Eike Drost, Holger Krämer',
+    'author_email' => 'christian.platt@pharmaline.de',
+    'author_company' => 'Pharmaline',
+    'state' => 'stable',
+    'clearCacheOnLoad' => true,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.0.0-13.99.99',
+            'php' => '8.4.0-8.99.99',
+            'extbase' => '13.0.0-13.99.99',
+            'felogin' => '13.0.0-13.99.99',
+            'fluid' => '13.4.0-13.99.99',
+            'frontend' => '13.0.0-13.99.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
